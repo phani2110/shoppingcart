4fef9aa380d9e1b0d2e705ee74683df55345f415
phani2110 <kcpvarma21@gmail.com>
2023-09-30 14:15:41 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 1450,
+    desc: "Men Regular Fit Solid Spread Collar Casual Shirt",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 2000,
+    desc: "Men Regular Fit Solid Button Down Collar Formal Shirts",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 650,
+    desc: "Men Solid Round Neck Pure Cotton White T-Shirt",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 3000,
+    desc: "Men SLIM FIT Solid Suit",
+    img: "images/img-4.jpg",
+  },
+];
